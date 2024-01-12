cd5fd8d6929c9cd38598828cc0503f2494b8d648
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-12 12:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+ function convertToUppercase() {
+            // Get the input element
+            var inputElement = document.getElementById("fname");
+
+            // Convert the entered text to uppercase
+            inputElement.value = inputElement.value.toUpperCase();
+        }
+
+        // Add event listener to the input field
+        document.getElementById("fname").addEventListener("blur",convertToUppercase);
+    
+    
